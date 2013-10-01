f6139c154e91192d1747cd81e0424c32e8c71ada
adria <araidz@gmail.com>
2013-10-01 16:39:52 -0400

    replaced some files

---
+++ Final/MindwaveServoFinal/MindwaveServoFinal.ino
@@ -0,0 +1,122 @@
+#include <VarSpeedServo.h>
+
+// control
+int potPin = A0;
+int switchPin = 2;
+int ledPin = 12;
+
+// servo
+VarSpeedServo servo; 
+int servoPin = 10;
+int servoSpeed = 200;
+
+// mindwave
+#define BAUDRATE 57600  // for bluetooth and serial debug
+byte generatedChecksum = 0;
+byte checksum = 0; 
+int payloadLength = 0;
+byte payloadData[64] = {0};
+long lastReceivedPacket = 0;
+boolean bigPacket = false;
+
+// mindwave output
+byte poorQuality = 250;
+byte attention = 0;
+byte meditation = 0;
+byte lastPoorQuality = 250;
+byte lastAttention = 0;
+byte lastMeditation = 0;
+
+// installation logic
+boolean bDebug = false;
+boolean useMeditation = false;
+int activationThresh = 60;
+float damping = 0.025;
+unsigned long dampFreq = 17; //about 60fps
+unsigned long lastDamp = 0;
+unsigned long timeOverThresh = 0;
+unsigned long timeUnderThresh = 0;
+unsigned long timeGrown = 0;
+unsigned long lastMillis;
+int state = 0; // state machibne - 0:waiting to grow, 1:grown, 2: waiting to die 
+String states[] = {
+  "0- waiting to grow", "1- growing", "2- waiting to die"};
+float output = 0;
+
+//---------------------------------------------------------------------------------
+
+void setup() 
+{ 
+  Serial.begin(BAUDRATE);
+  servo.attach(servoPin, 0, 180);
+} 
+
+void loop() 
+{ 
+  readControl();
+  parseMindwave();
+  processNewData();
+  calculateOutput();
+
+  if(bDebug && bigPacket){
+    physicalDebug();
+    debugInput();
+    //debugLogic();
+  }
+  outputToServo(output);
+} 
+
+void readControl(){
+  //activationThresh = map(analogRead(potPin), 0, 1023, 0, 100);
+  servoSpeed = map(analogRead(potPin), 0, 1023, 0, 255);
+  useMeditation = digitalRead(switchPin);
+}
+
+void physicalDebug(){
+  if(poorQuality == 0) digitalWrite(ledPin, HIGH);
+  else digitalWrite(ledPin, LOW);
+}
+
+void debugInput(){
+  Serial.print("PoorQuality: ");
+  Serial.print(poorQuality, DEC);
+  Serial.print(" Attention: ");
+  Serial.print(attention, DEC);
+  Serial.print(" Meditation: ");
+  Serial.print(meditation, DEC);
+  Serial.print(" Time since last packet: ");
+  Serial.print((millis() - lastReceivedPacket)/1000.0, 3);
+  lastReceivedPacket = millis();
+  Serial.print("\n");
+}
+
+void debugLogic(){
+  Serial.print("state: ");
+  Serial.print(state);
+  Serial.print(" timeUnderThresh: ");
+  Serial.print(timeUnderThresh);
+  Serial.print(" timeOverThresh: ");
+  Serial.print(timeOverThresh);
+  Serial.print(" timeGrown: ");
+  Serial.print(timeGrown);
+  Serial.print(" output: ");
+  Serial.println(output);
+  Serial.println(" ");
+}
+
+//---------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Final/MindwaveServoFinal/Stepper.ino
+++
@@ -1,5 +0,0 @@
-
-void outputWithStepper(float out){
-  stepper.moveTo(map(out, 0, 100, 0, stepperCicle));
-  stepper.run();
-}
